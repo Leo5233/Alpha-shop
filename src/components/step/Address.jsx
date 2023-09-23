a6631121a7926dca96e0843bac11004f80a435d1
Leo5233 <leohsieh53@gmail.com>
2023-09-23 21:26:36 +0800

    Wrap city list in address part with map function

--- src/components/step/Address.jsx
+++ src/components/step/Address.jsx
@@ -3,6 +3,24 @@ export default function address({step}) {
   if (step !== 1) {
     addressclass = 'd-none'
   }
+  const cityList = ["KLU-基隆市",
+    "TPH-新北市","TPE-臺北市","TYC-桃園市","HSH-新竹縣","HSC-新竹市","MAC-苗栗市","MAL-苗栗縣","TXG-臺中市",
+    "CWH-彰化縣","CWS-彰化市","NTC-南投市","NTO-南投縣","YLH-雲林縣","CHY-嘉義縣","CYI-嘉義市","TNN-臺南市",
+    "KHH-高雄市","IUH-屏東縣","PTS-屏東市","ILN-宜蘭縣","ILC-宜蘭市","HWA-花蓮縣","HWC-花蓮市","TTC-臺東市",
+    "TTT-臺東縣","PEH-澎湖縣","KMN-金門縣","LNN-連江縣"]
+  function Select({cityList}) {
+    const listItem = cityList.map( city => {
+      const name = city.split('-')[1]
+      const shortName = city.split('-')[0]
+      return (<option value={shortName}>{name}</option>)
+    })
+    return (
+    <select required>
+      <option value="">請選擇縣市</option>
+      {listItem}
+    </select>
+    )
+  }
   return (
     <div className={addressclass}>
       <htmlForm className = "col col-12" data-phase="address" >
@@ -38,38 +56,7 @@ export default function address({step}) {
             <div className="input-group city input-w-lg-2 input-w-sm-full">
               <div className="input-label">縣市</div>
               <div className="select-container">
-                <select required>
-                  <option value="">請選擇縣市</option>
-                  <option value="KLU">基隆市</option>
-                  <option value="TPH">新北市</option>
-                  <option value="TPE">臺北市</option>
-                  <option value="TYC">桃園市</option>
-                  <option value="HSH">新竹縣</option>
-                  <option value="HSC">新竹市</option>
-                  <option value="MAC">苗栗市</option>
-                  <option value="MAL">苗栗縣</option>
-                  <option value="TXG">臺中市</option>
-                  <option value="CWH">彰化縣</option>
-                  <option value="CWS">彰化市</option>
-                  <option value="NTC">南投市</option>
-                  <option value="NTO">南投縣</option>
-                  <option value="YLH">雲林縣</option>
-                  <option value="CHY">嘉義縣</option>
-                  <option value="CYI">嘉義市</option>
-                  <option value="TNN">臺南市</option>
-                  <option value="KHH">高雄市</option>
-                  <option value="IUH">屏東縣</option>
-                  <option value="PTS">屏東市</option>
-                  <option value="ILN">宜蘭縣</option>
-                  <option value="ILC">宜蘭市</option>
-                  <option value="HWA">花蓮縣</option>
-                  <option value="HWC">花蓮市</option>
-                  <option value="TTC">臺東市</option>
-                  <option value="TTT">臺東縣</option>
-                  <option value="PEH">澎湖縣</option>
-                  <option value="KMN">金門縣</option>
-                  <option value="LNN">連江縣</option>
-                </select>
+                <Select cityList={cityList}/>
               </div>
             </div>
             <div className="input-group address input-w-lg-4 input-w-sm-full">
@@ -81,4 +68,5 @@ export default function address({step}) {
       </htmlForm >
     </div>
 );
+
 }
